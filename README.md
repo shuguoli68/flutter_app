4a693678f981dbec75ee70c8fe4ae2e3799ee74a
shu <shuguoli68@163.com>
2019-03-23 11:33:16 +0800

    第一次提交Flutter项目代码

---
+++ README.md
@@ -0,0 +1,21 @@
+# My Flutter project
+Trying to develop a mobile app using Flutter was a bit uncomfortable at first, but the development efficiency did improve.
+(尝试使用Flutter开发移动应用程序，刚开始还是有点不适应，但是开发效率确实提高了。)
+
+
+# flutter_app
+
+A new Flutter application.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.io/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: 'Welcome',
+      home: new Scaffold(
+        body: new RandomWords(),
+      )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+    @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords>{
+  final _suggesstions = <WordPair>[];
+  final _bigfont = new TextStyle(fontSize: 18.0);
+  final _save = new Set<WordPair>();
+  @override
+  Widget build(BuildContext context) {
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text('列表显示'),
+      actions: <Widget>[
+        new IconButton(icon: new Icon(Icons.list), onPressed: _pushSave),
+        new IconButton(icon: new Icon(Icons.account_circle), onPressed: _pushMy)
+      ],
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+  Widget _buildRow(WordPair w){
+    final _isSaved = _save.contains(w);
+    return new ListTile(
+      title: new Text(
+        w.asPascalCase,
+        style: _bigfont,
+      ),
+      trailing: new Icon(
+        _isSaved?Icons.favorite:Icons.favorite_border,
+        color: _isSaved?Colors.red:null,
+      ),
+      onTap: (){
+        setState(() {
+          if(_isSaved){
+            _save.remove(w);
+          }else{
+            _save.add(w);
+          }
+        });
+      },
+    );
+  }
+  Widget _buildSuggestions(){
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context,i){
+        if(i.isOdd){
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          return new Divider();
+        }
+        final index = i ~/ 2;
+        if(index >= _suggesstions.length){//列表中最后一个
+          _suggesstions.addAll(generateWordPairs().take(5));
+        }
+        return _buildRow(_suggesstions[index]);
+        }
+    );
+  }
+
+  void _pushSave(){//第二页面：已收藏页面
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder:(context){
+              final tiles = _save.map(
+                      (pa){
+                    return ListTile(
+                      title: new Text(
+                        pa.asPascalCase,
+                        style: _bigfont,
+                      ),
+                    );
+                  }
+              );
+              final divided = ListTile
+                  .divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList();
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: new Text('已收藏'),
+                ),
+                body: new ListView(children: divided,),
+              );
+            }
+        )
+    );
+  }
+  void _pushMy(){//第三页面：个人主页
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context){
+      return new Scaffold(
+        appBar: new AppBar(title: new Text('个人主页'),),
+        body: new ListView(
+          children: <Widget>[
+            new Image.asset('images/user_ba.png',width: 600.0,height: 240.0,fit: BoxFit.cover,),
+            titleSection,
+            buttonSection,
+            txtSection,
+          ],
+        ),
+      );
+    }));
+  }
+
+  Widget titleSection = new Container(
+    padding: const EdgeInsets.all(32.0),
+    child:new Row(
+      children: [
+        new Expanded(
+          child:new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: new Text('Oeschinen Lake Campground',style: new TextStyle(fontWeight: FontWeight.bold),),
+              ),
+              new Text('Kandersteg, Switzerland',style: new TextStyle(color: Colors.grey[500]),)
+            ],
+          ),
+        ),
+        new Icon(Icons.star,color: Colors.red[500],),
+        new Text('50')
+      ]
+    )
+  );
+
+  static Column addColumn(IconData icon, String label){
+    Color color = Colors.deepPurpleAccent;
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Icon(icon,color: color,),
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color
+            ),
+          ),
+        )
+      ],
+    );
+  }
+  Widget buttonSection = new Container(
+    child: new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        addColumn(Icons.call, 'CALL'),
+        addColumn(Icons.near_me, 'ROUTE'),
+        addColumn(Icons.share, 'SHARE')
+      ],
+    )
+  );
+
+  Widget txtSection = new Container(
+    padding: const EdgeInsets.all(32.0),
+    child: new Text('''
+    Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+    ''',
+      softWrap: true,)
+  );
+}
