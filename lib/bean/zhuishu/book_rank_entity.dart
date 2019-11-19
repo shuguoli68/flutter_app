d05e19df8e7cf29403d1eda8c1b3fa9152907e3f
shu <shuguoli68@163.com>
2019-11-19 17:51:06 +0800

    添加排行榜

--- lib/bean/zhuishu/book_rank_entity.dart
+++ lib/bean/zhuishu/book_rank_entity.dart
@@ -3,27 +3,27 @@
  * http://api.zhuishushenqi.com/ranking/gender
  */
 class BookRankEntity {
-	List<BookRankEpub> epub;
-	List<BookRankFemale> female;
+	List<BookRankItem> epub;
+	List<BookRankItem> female;
 	bool ok;
-	List<BookRankMale> male;
-	List<BookRankPicture> picture;
+	List<BookRankItem> male;
+	List<BookRankItem> picture;
 
 	BookRankEntity({this.epub, this.female, this.ok, this.male, this.picture});
 
 	BookRankEntity.fromJson(Map<String, dynamic> json) {
 		if (json['epub'] != null) {
-			epub = new List<BookRankEpub>();(json['epub'] as List).forEach((v) { epub.add(new BookRankEpub.fromJson(v)); });
+			epub = new List<BookRankItem>();(json['epub'] as List).forEach((v) { epub.add(new BookRankItem.fromJson(v)); });
 		}
 		if (json['female'] != null) {
-			female = new List<BookRankFemale>();(json['female'] as List).forEach((v) { female.add(new BookRankFemale.fromJson(v)); });
+			female = new List<BookRankItem>();(json['female'] as List).forEach((v) { female.add(new BookRankItem.fromJson(v)); });
 		}
 		ok = json['ok'];
 		if (json['male'] != null) {
-			male = new List<BookRankMale>();(json['male'] as List).forEach((v) { male.add(new BookRankMale.fromJson(v)); });
+			male = new List<BookRankItem>();(json['male'] as List).forEach((v) { male.add(new BookRankItem.fromJson(v)); });
 		}
 		if (json['picture'] != null) {
-			picture = new List<BookRankPicture>();(json['picture'] as List).forEach((v) { picture.add(new BookRankPicture.fromJson(v)); });
+			picture = new List<BookRankItem>();(json['picture'] as List).forEach((v) { picture.add(new BookRankItem.fromJson(v)); });
 		}
 	}
 
@@ -46,35 +46,35 @@ class BookRankEntity {
 	}
 }
 
-class BookRankEpub {
-	String cover;
-	String sId;
-	String shortTitle;
-	String title;
-	bool collapse;
+//class BookRankEpub {
+//	String cover;
+//	String sId;
+//	String shortTitle;
+//	String title;
+//	bool collapse;
+//
+//	BookRankEpub({this.cover, this.sId, this.shortTitle, this.title, this.collapse});
+//
+//	BookRankEpub.fromJson(Map<String, dynamic> json) {
+//		cover = json['cover'];
+//		sId = json['_id'];
+//		shortTitle = json['shortTitle'];
+//		title = json['title'];
+//		collapse = json['collapse'];
+//	}
+//
+//	Map<String, dynamic> toJson() {
+//		final Map<String, dynamic> data = new Map<String, dynamic>();
+//		data['cover'] = this.cover;
+//		data['_id'] = this.sId;
+//		data['shortTitle'] = this.shortTitle;
+//		data['title'] = this.title;
+//		data['collapse'] = this.collapse;
+//		return data;
+//	}
+//}
 
-	BookRankEpub({this.cover, this.sId, this.shortTitle, this.title, this.collapse});
-
-	BookRankEpub.fromJson(Map<String, dynamic> json) {
-		cover = json['cover'];
-		sId = json['_id'];
-		shortTitle = json['shortTitle'];
-		title = json['title'];
-		collapse = json['collapse'];
-	}
-
-	Map<String, dynamic> toJson() {
-		final Map<String, dynamic> data = new Map<String, dynamic>();
-		data['cover'] = this.cover;
-		data['_id'] = this.sId;
-		data['shortTitle'] = this.shortTitle;
-		data['title'] = this.title;
-		data['collapse'] = this.collapse;
-		return data;
-	}
-}
-
-class BookRankFemale {
+class BookRankItem {
 	String cover;
 	String totalRank;
 	String monthRank;
@@ -83,9 +83,9 @@ class BookRankFemale {
 	String title;
 	bool collapse;
 
-	BookRankFemale({this.cover, this.totalRank, this.monthRank, this.sId, this.shortTitle, this.title, this.collapse});
+	BookRankItem({this.cover, this.totalRank, this.monthRank, this.sId, this.shortTitle, this.title, this.collapse});
 
-	BookRankFemale.fromJson(Map<String, dynamic> json) {
+	BookRankItem.fromJson(Map<String, dynamic> json) {
 		cover = json['cover'];
 		totalRank = json['totalRank'];
 		monthRank = json['monthRank'];
@@ -108,64 +108,64 @@ class BookRankFemale {
 	}
 }
 
-class BookRankMale {
-	String cover;
-	String totalRank;
-	String monthRank;
-	String sId;
-	String shortTitle;
-	String title;
-	bool collapse;
-
-	BookRankMale({this.cover, this.totalRank, this.monthRank, this.sId, this.shortTitle, this.title, this.collapse});
-
-	BookRankMale.fromJson(Map<String, dynamic> json) {
-		cover = json['cover'];
-		totalRank = json['totalRank'];
-		monthRank = json['monthRank'];
-		sId = json['_id'];
-		shortTitle = json['shortTitle'];
-		title = json['title'];
-		collapse = json['collapse'];
-	}
-
-	Map<String, dynamic> toJson() {
-		final Map<String, dynamic> data = new Map<String, dynamic>();
-		data['cover'] = this.cover;
-		data['totalRank'] = this.totalRank;
-		data['monthRank'] = this.monthRank;
-		data['_id'] = this.sId;
-		data['shortTitle'] = this.shortTitle;
-		data['title'] = this.title;
-		data['collapse'] = this.collapse;
-		return data;
-	}
-}
-
-class BookRankPicture {
-	String cover;
-	String sId;
-	String shortTitle;
-	String title;
-	bool collapse;
-
-	BookRankPicture({this.cover, this.sId, this.shortTitle, this.title, this.collapse});
-
-	BookRankPicture.fromJson(Map<String, dynamic> json) {
-		cover = json['cover'];
-		sId = json['_id'];
-		shortTitle = json['shortTitle'];
-		title = json['title'];
-		collapse = json['collapse'];
-	}
-
-	Map<String, dynamic> toJson() {
-		final Map<String, dynamic> data = new Map<String, dynamic>();
-		data['cover'] = this.cover;
-		data['_id'] = this.sId;
-		data['shortTitle'] = this.shortTitle;
-		data['title'] = this.title;
-		data['collapse'] = this.collapse;
-		return data;
-	}
-}
+//class BookRankMale {
+//	String cover;
+//	String totalRank;
+//	String monthRank;
+//	String sId;
+//	String shortTitle;
+//	String title;
+//	bool collapse;
+//
+//	BookRankMale({this.cover, this.totalRank, this.monthRank, this.sId, this.shortTitle, this.title, this.collapse});
+//
+//	BookRankMale.fromJson(Map<String, dynamic> json) {
+//		cover = json['cover'];
+//		totalRank = json['totalRank'];
+//		monthRank = json['monthRank'];
+//		sId = json['_id'];
+//		shortTitle = json['shortTitle'];
+//		title = json['title'];
+//		collapse = json['collapse'];
+//	}
+//
+//	Map<String, dynamic> toJson() {
+//		final Map<String, dynamic> data = new Map<String, dynamic>();
+//		data['cover'] = this.cover;
+//		data['totalRank'] = this.totalRank;
+//		data['monthRank'] = this.monthRank;
+//		data['_id'] = this.sId;
+//		data['shortTitle'] = this.shortTitle;
+//		data['title'] = this.title;
+//		data['collapse'] = this.collapse;
+//		return data;
+//	}
+//}
+//
+//class BookRankPicture {
+//	String cover;
+//	String sId;
+//	String shortTitle;
+//	String title;
+//	bool collapse;
+//
+//	BookRankPicture({this.cover, this.sId, this.shortTitle, this.title, this.collapse});
+//
+//	BookRankPicture.fromJson(Map<String, dynamic> json) {
+//		cover = json['cover'];
+//		sId = json['_id'];
+//		shortTitle = json['shortTitle'];
+//		title = json['title'];
+//		collapse = json['collapse'];
+//	}
+//
+//	Map<String, dynamic> toJson() {
+//		final Map<String, dynamic> data = new Map<String, dynamic>();
+//		data['cover'] = this.cover;
+//		data['_id'] = this.sId;
+//		data['shortTitle'] = this.shortTitle;
+//		data['title'] = this.title;
+//		data['collapse'] = this.collapse;
+//		return data;
+//	}
+//}
--- lib/widget/main/book_rank.dart
+++ lib/widget/main/book_rank.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/bean/zhuishu/book_rank_entity.dart';
 import 'package:flutter_app/global/my_public.dart';
 import 'package:flutter_app/global/common.dart';
+import 'package:flutter_app/util/HttpUtils.dart';
+import 'package:flutter_tableview/flutter_tableview.dart';
+
+import '../../entity_factory.dart';
 
 class BookRank extends StatefulWidget{
   @override
@@ -8,11 +13,133 @@ class BookRank extends StatefulWidget{
 }
 
 class _BookRank extends State<BookRank>{
+
+  BookRankEntity data;
+
+  // 有多少个组
+  int sectionCount = 4;
+  int section = 0;
+  int row = 0;
+
+  _getData () async {
+    var result = await HttpUtils.request(
+        '/ranking/gender',
+        method: HttpUtils.GET,
+        data: {
+        }
+    );
+    setState(() {
+      data = EntityFactory.generateOBJ(result);
+    });
+  }
+
+  // 每组有多少行（每组有多少个cell item）
+  int _itemCount(int section) {
+    if (section == 1) {
+      return data.male.length;
+    } else if (section == 2) {
+      return data.female.length;
+    } else if (section == 3) {
+      return data.epub.length;
+    }else {
+      return data.picture.length;
+    }
+  }
+
+  List<BookRankItem> _itemOne(int section) {
+    if (section == 1) {
+      return data.male;
+    } else if (section == 2) {
+      return data.female;
+    } else if (section == 3) {
+      return data.epub;
+    }else {
+      return data.picture;
+    }
+  }
+
+
+  // 创建每组的header
+  Widget _sectionHeaderBuilder(BuildContext context, int section) {
+    String title;
+    switch(section){
+      case 1:
+        title = '男生';
+        break;
+      case 2:
+        title = '女生';
+        break;
+      case 3:
+        title = '出版';
+        break;
+      default:
+        title = '精选';
+        break;
+    }
+    return InkWell(
+      onTap: () {
+        print('click section header. -> section:$section');
+      },
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: 16.0),
+        color: Color.fromRGBO(220, 220, 220, 1),
+        height: 100,
+        child: Text(title),
+      ),
+    );
+  }
+
+  // 根据 section 和 row, 创建对应的item
+  Widget _cellBuilder(BuildContext context, int section, int row) {
+    return InkWell(
+      onTap: () {
+        this.section = section;
+        this.row = row;
+        print('click cell item. -> section:$section row:$row');
+      },
+      child: Flex(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.all(5.0),
+            child: ClipOval(
+              child: Image.network(_itemOne(section)[row].cover,height: 30,width: 30,fit: BoxFit.fitHeight,),
+            ),
+          ),
+          Expanded(child: Text(_itemOne(section)[row].title,maxLines: 1,overflow: TextOverflow.ellipsis),)
+        ],
+      ),
+    );
+  }
+
+  // section header widget 的高度
+  double _sectionHeaderHeight(BuildContext context, int section) {
+    return 30.0;
+  }
+
+  // cell item widget 的高度
+  double _cellHeight(BuildContext context, int section, int row) {
+    return 60.0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('BookRank');
-    return Scaffold(
-      body: Text('BookRank'),
+    return Container(
+      //FlutterTableView
+      child: FlutterTableView(
+        sectionCount: sectionCount,
+        rowCountAtSection: _itemCount,
+        sectionHeaderBuilder: _sectionHeaderBuilder,
+        cellBuilder: _cellBuilder,
+        sectionHeaderHeight: _sectionHeaderHeight,
+        cellHeight: _cellHeight,
+      ),
     );
   }
 }
